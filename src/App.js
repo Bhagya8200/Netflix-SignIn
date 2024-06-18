47d1ba4cb8b27a17ed84cda937003d25776ac472
Bhagya8200 <neelapubhagyasree8200@gmail.com>
2024-06-19 00:14:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>    
+    <div>
+   <div className="container">
+    <h2>Sign In</h2>
+    <div>
+        <input type="text" placeholder="Email or mobile number" size="30"/><br/><br/>
+        <input type="password" placeholder="Password" size="30"/><br/>
+    </div>    
+    <button id="btn1" >Sign In</button>
+    <p>OR</p>
+    <button id="btn2" >Use a sign-in code</button>
+    <p id="forgot-pass">Forgot password?</p>
+    <div className="checkbox">
+        <input type="checkbox" id="remember"/>
+        <label for="remember">Remember me</label>
     </div>
+    <p id="text">New to Netflix? <span>Sign up now.</span></p>
+    <p id="bottom-text">This page is protected by Google reCAPTCHA to ensure you're not a bot.<a href="google.com">Learn more</a></p>
+   </div> 
+</div>
+    </>
   );
 }
 
